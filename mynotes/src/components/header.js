acf9e00c76f9917b04042513a7321501d5d31d8d
Ustadmohsin <mohsinustad09@gmail.com>
2025-08-11 13:18:08 +0530

    Update header.js

--- mynotes/src/components/header.js
+++ mynotes/src/components/header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="app-header">
-            <h1>My Notes from AIZTS-infotech solapur</h1>
+            <h1>My Notes from AIZTS-infotech solapur .....</h1>
         </div>
     )
 }
